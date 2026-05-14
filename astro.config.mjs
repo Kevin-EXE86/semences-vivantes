319578905a6cb4464cce6910ebcc933dc697bf5a
kevin fieffe <kevinfieffe@gmail.com>
2026-05-14 10:28:41 +0200

    debugg issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://semences-vivantes.fr',
-  compressHTML: true,
+  site: 'https://your-domain.com', // <- set this to your live site URL
   integrations: [sitemap()],
+  // ...other config
 });
