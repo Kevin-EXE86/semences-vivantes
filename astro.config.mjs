cbed16a22dbf62131f7e36d04bd3b9b5b6df6c73
kevin fieffe <kevinfieffe@gmail.com>
2026-05-14 11:13:42 +0200

    debugg issue by removing sitemap importation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://semences-vivantes.fr',
