e0606d5b16dcf0040f74728555438dba31e0b9fb
kevin fieffe <kevinfieffe@gmail.com>
2026-05-14 10:13:39 +0200

    Ajout step 3 - SEO, pages légales, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://semences-vivantes.fr',
   compressHTML: true,
+  integrations: [sitemap()],
 });
