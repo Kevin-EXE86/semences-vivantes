0e94388f8e3807c1119a7b03bc9660aceb02629b
kevin fieffe <kevinfieffe@gmail.com>
2026-05-18 11:39:20 +0200

    reactivation sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-// import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://semences-vivantes.fr',
   compressHTML: true,
-  //integrations: [sitemap()],
+  integrations: [sitemap()],
 });
